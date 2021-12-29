46e6e2beb29c1f2acadea2605b50d616a65199b0
zernonia <59365435+zernonia@users.noreply.github.com>
2021-12-29 13:26:02 +0800

    fix: publish

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -2,10 +2,10 @@
 
 GIT_TAG=$(git describe --abbrev=0 --tags)
 
-$(cd example && npm install -f vue-notion@latest)
+$(cd demo && npm install -f vue3-notion@latest)
 
 git add --all
-git commit -m 'Update example'
+git commit -m 'Update demo'
 git push --tags
 
 gh release create $GIT_TAG -t "$GIT_TAG"
