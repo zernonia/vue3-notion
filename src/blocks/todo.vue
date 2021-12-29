cc06377aca77c0cc19e39f85971203f2600b6b2f
zernonia <59365435+zernonia@users.noreply.github.com>
2021-12-29 19:46:46 +0800

    fix: checkbox rendering

--- src/blocks/todo.vue
+++ src/blocks/todo.vue
@@ -3,10 +3,12 @@
 // TODO: add strikethrough
 import { useNotionBlock, defineNotionProps } from "@/lib/blockable"
 import NotionTextRenderer from "@/blocks/helpers/text-renderer.vue"
+import { computed } from "vue"
 
 const props = defineProps({ ...defineNotionProps })
 //@ts-ignore
 const { title, properties, pass } = useNotionBlock(props)
+const checked = computed(() => properties.value?.checked?.[0] == "Yes")
 </script>
 
 <script lang="ts">
@@ -17,14 +19,8 @@ export default {
 
 <template>
   <label class="notion-to-do-item">
-    <input
-      type="checkbox"
-      class="notion-property-checkbox"
-      :value="title"
-      :checked="properties.checked"
-      disabled="true"
-    />
-    <label :class="{ 'notion-to-do-checked': properties.checked }">
+    <input type="checkbox" class="notion-property-checkbox" :value="title" :checked="checked" disabled="true" />
+    <label :class="{ 'notion-to-do-checked': checked }">
       <NotionTextRenderer :text="title" v-bind="pass" />
     </label>
   </label>
