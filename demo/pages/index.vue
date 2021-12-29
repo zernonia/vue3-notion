96cc721c712920ee98dfd9594214eca742b56b72
zernonia <59365435+zernonia@users.noreply.github.com>
2021-12-29 23:44:35 +0800

    update: demo props

--- demo/pages/index.vue
+++ demo/pages/index.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 import { useNuxtApp } from "#app"
+import { mapPageUrl, pageLinkOptions } from "@/props"
 
 const { $notion } = useNuxtApp()
-const { data } = await useAsyncData("notion", () => $notion.getPageBlocks("2e22de6b770e4166be301490f6ffd420"))
+const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("2e22de6b770e4166be301490f6ffd420"))
 
 // react-notion tester: 2e22de6b770e4166be301490f6ffd420
 // equation tester: 2a1d5226d68246deba627012081693f9
 // table tester: bd1de400a8b349dc824f4f00e61d0797
 // todo tester: 235057194b954a60ace89c052a65d102
-
-const mapPageUrl = (pageId: String) => {
-  return `/page/${pageId}`
-}
 </script>
 
 <template>
   <div>
-    <NotionRenderer :blockMap="data" fullPage prism katex :mapPageUrl="mapPageUrl" />
+    <NotionRenderer :blockMap="data" fullPage prism katex :mapPageUrl="mapPageUrl" :pageLinkOptions="pageLinkOptions" />
   </div>
 </template>
