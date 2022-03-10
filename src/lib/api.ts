b57c317eb845ec0d488c757604e1953a1230c2d4
zernonia <zernonia@gmail.com>
2022-03-10 21:42:57 +0800

    fix catch fetch error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,10 +4,12 @@ const getPageTable = async (pageId: string, apiUrl = "https://api.vue-notion.wor
   await fetch(`${apiUrl}/table/${pageId}`)
     .then((res) => res.json())
     .then((data) => data as NotionBlockProps)
+    .catch((err) => err)
 
 const getPageBlocks = async (pageId: string, apiUrl = "https://api.vue-notion.workers.dev/v1") =>
   await fetch(`${apiUrl}/page/${pageId}`)
     .then((res) => res.json())
     .then((data) => data as NotionBlockProps)
+    .catch((err) => err)
 
 export { getPageTable, getPageBlocks }
