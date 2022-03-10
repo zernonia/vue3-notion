ef27a677311933087ee2ec939de7c595defe8e97
zernonia <zernonia@gmail.com>
2022-03-10 21:55:20 +0800

    fix function return types

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,15 @@
-import { NotionBlockProps } from "@/lib/types"
+import { BlockMap } from "@/lib/types"
 
 const getPageTable = async (pageId: string, apiUrl = "https://api.vue-notion.workers.dev/v1") =>
   await fetch(`${apiUrl}/table/${pageId}`)
     .then((res) => res.json())
-    .then((data) => data as NotionBlockProps)
+    .then((data) => data as BlockMap)
     .catch((err) => err)
 
 const getPageBlocks = async (pageId: string, apiUrl = "https://api.vue-notion.workers.dev/v1") =>
   await fetch(`${apiUrl}/page/${pageId}`)
     .then((res) => res.json())
-    .then((data) => data as NotionBlockProps)
+    .then((data) => data as BlockMap)
     .catch((err) => err)
 
 export { getPageTable, getPageBlocks }
