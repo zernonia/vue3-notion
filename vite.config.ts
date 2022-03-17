b278e84379b36146e301b9b4630330ce20e6f465
zernonia <zernonia@gmail.com>
2022-03-17 23:31:46 +0800

    use vite lib buildmode

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "index",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: "Vue",
+        },
+        exports: "named",
+        dir: "dist",
+      },
     },
   },
 })
