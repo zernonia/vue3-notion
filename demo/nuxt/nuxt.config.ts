baa77c824be3813223167351c8f33a04c2efa453
zernonia <zernonia@gmail.com>
2022-04-26 11:11:04 +0800

    update favicon

--- demo/nuxt/nuxt.config.ts
+++ demo/nuxt/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   meta: {
     title: "Nuxt3 Notion",
     meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
-    link: [{ rel: "icon", type: "image/svg", href: "/logo.svg" }],
+    link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
   },
   buildModules: ["vue3-notion/nuxt"],
 })
