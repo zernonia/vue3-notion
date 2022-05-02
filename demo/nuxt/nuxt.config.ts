34a50237912261147549c7bd95c632f69311d97e
zernonia <zernonia@gmail.com>
2022-05-02 11:12:24 +0800

    chore: bump notion to 0.1.29, update nuxt config with css option

--- demo/nuxt/nuxt.config.ts
+++ demo/nuxt/nuxt.config.ts
@@ -6,5 +6,5 @@ export default defineNuxtConfig({
     meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
     link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
   },
-  buildModules: ["vue3-notion/nuxt"],
+  modules: [["vue3-notion/nuxt", { css: true }]],
 })
