93b049c12ac85629058783653f22d29514983a4c
zernonia <zernonia@gmail.com>
2022-05-03 13:14:42 +0800

    fix vite bundling prismjs language

--- src/components/block.vue
+++ src/components/block.vue
@@ -18,46 +18,7 @@ import NotionTodo from "@/blocks/todo.vue"
 import NotionToggle from "@/blocks/toggle.vue"
 import NotionTableOfContents from "@/blocks/table-of-contents.vue"
 
-const NotionCode = defineAsyncComponent(() =>
-  import("@/blocks/code.vue").then(async (m) => {
-    await Promise.all([
-      import("prismjs/components/prism-markup-templating"),
-      import("prismjs/components/prism-markup"),
-      import("prismjs/components/prism-bash"),
-      import("prismjs/components/prism-c"),
-      import("prismjs/components/prism-cpp"),
-      import("prismjs/components/prism-csharp"),
-      import("prismjs/components/prism-docker"),
-      import("prismjs/components/prism-java"),
-      import("prismjs/components/prism-js-templates"),
-      import("prismjs/components/prism-typescript"),
-      import("prismjs/components/prism-coffeescript"),
-      import("prismjs/components/prism-diff"),
-      import("prismjs/components/prism-git"),
-      import("prismjs/components/prism-go"),
-      import("prismjs/components/prism-graphql"),
-      import("prismjs/components/prism-handlebars"),
-      import("prismjs/components/prism-json"),
-      import("prismjs/components/prism-less"),
-      import("prismjs/components/prism-makefile"),
-      import("prismjs/components/prism-markdown"),
-      import("prismjs/components/prism-objectivec"),
-      import("prismjs/components/prism-ocaml"),
-      import("prismjs/components/prism-python"),
-      import("prismjs/components/prism-reason"),
-      import("prismjs/components/prism-rust"),
-      import("prismjs/components/prism-sass"),
-      import("prismjs/components/prism-scss"),
-      import("prismjs/components/prism-solidity"),
-      import("prismjs/components/prism-sql"),
-      import("prismjs/components/prism-stylus"),
-      import("prismjs/components/prism-swift"),
-      import("prismjs/components/prism-wasm"),
-      import("prismjs/components/prism-yaml"),
-    ])
-    return m
-  })
-)
+const NotionCode = defineAsyncComponent(() => import("@/blocks/code.vue"))
 
 const props = defineProps({ ...defineNotionProps })
 //@ts-ignore
