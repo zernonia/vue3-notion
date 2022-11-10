80fb0384de82d8a606424dd57ceceed7e24b5144
zernonia <zernonia@gmail.com>
2022-11-11 06:56:56 +0800

    update nuxt.config

--- demo/nuxt/nuxt.config.ts
+++ demo/nuxt/nuxt.config.ts
@@ -1,10 +1,10 @@
-import { defineNuxtConfig } from "nuxt"
-
 export default defineNuxtConfig({
-  meta: {
-    title: "Nuxt3 Notion",
-    meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
-    link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
+  app: {
+    head: {
+      title: "Nuxt3 Notion",
+      meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
+      link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
+    },
   },
   modules: [["vue3-notion/nuxt", { css: true }]],
-})
+});
--- demo/nuxt/pages/index.vue
+++ demo/nuxt/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-const { $notion } = useNuxtApp()
-const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("4b2dc28a5df74034a943f8c8e639066a"))
-const { mapPageUrl, pageLinkOptions } = useProps()
+const { $notion } = useNuxtApp();
+const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("4b2dc28a5df74034a943f8c8e639066a"));
+const { mapPageUrl, pageLinkOptions } = useProps();
 </script>
 
 <template>
