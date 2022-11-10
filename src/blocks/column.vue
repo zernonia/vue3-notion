97568810881d2e00e14833fb6d37b08fa8a42167
zernonia <zernonia@gmail.com>
2022-11-11 05:18:55 +0800

    remove fragment-for-vue dependencies

--- src/blocks/column.vue
+++ src/blocks/column.vue
@@ -1,35 +1,32 @@
 <script setup lang="ts">
-import { computed } from "vue"
-import { Fragment } from "fragment-for-vue/vue3"
+import { computed } from "vue";
 
-const baseWidth = 46 // todo: add magic numbers to a config json
+const baseWidth = 46; // todo: add magic numbers to a config json
 
 const props = defineProps({
   format: { type: Object, required: true },
-})
+});
 
 const columnStyle = computed(() => {
-  const columns = Number((1 / props.format.column_ratio).toFixed(0))
-  const totalWidth = (columns - 1) * baseWidth
+  const columns = Number((1 / props.format.column_ratio).toFixed(0));
+  const totalWidth = (columns - 1) * baseWidth;
   return {
     width: `calc((100% - ${totalWidth}px) * ${props.format.column_ratio})`,
-  }
-})
+  };
+});
 
-const spacerStyle = computed(() => ({ width: `${baseWidth}px` }))
+const spacerStyle = computed(() => ({ width: `${baseWidth}px` }));
 </script>
 
 <script lang="ts">
 export default {
   name: "NotionColumn",
-}
+};
 </script>
 
 <template>
-  <Fragment>
-    <div class="notion-column" :style="columnStyle">
-      <slot />
-    </div>
-    <div class="notion-spacer" :style="spacerStyle" />
-  </Fragment>
+  <div class="notion-column" :style="columnStyle">
+    <slot />
+  </div>
+  <div class="notion-spacer" :style="spacerStyle" />
 </template>
