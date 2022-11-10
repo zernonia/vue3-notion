df71a81a6f25f936563d28729581be7f7e927ac0
zernonia <zernonia@gmail.com>
2022-11-11 06:45:32 +0800

    update nuxt plugin

--- nuxt/index.ts
+++ nuxt/index.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit"
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
 
 export default defineNuxtModule({
   meta: {
@@ -9,19 +9,19 @@ export default defineNuxtModule({
     },
   },
   setup(options, nuxt) {
-    const { resolve } = createResolver(import.meta.url)
-    addPlugin(resolve("./plugin"))
+    const { resolve } = createResolver(import.meta.url);
+    addPlugin(resolve("./plugin"));
 
-    const notionDeps = ["vue3-notion, fragment-for-vue", "katex", "prismjs"]
+    const notionDeps = ["vue3-notion", "katex", "prismjs"];
 
     notionDeps.forEach((dep) => {
-      nuxt.options.build.transpile.push(dep)
-    })
+      nuxt.options.build.transpile.push(dep);
+    });
 
     if (options.css) {
-      nuxt.options.css.push("vue3-notion/dist/style.css")
-      nuxt.options.css.push("prismjs/themes/prism.css")
-      nuxt.options.css.push("katex/dist/katex.min.css")
+      nuxt.options.css.push("vue3-notion/dist/style.css");
+      nuxt.options.css.push("prismjs/themes/prism.css");
+      nuxt.options.css.push("katex/dist/katex.min.css");
     }
   },
-})
+});
